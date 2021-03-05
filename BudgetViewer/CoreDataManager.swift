dfa663ee474844cd6bd114d135f12a6153351511
Andrii Moisol <andrii.moisol@gmail.com>
2021-03-05 21:24:58 +0200

    Add CoreDataManager

---
+++ BudgetViewer/CoreDataManager.swift
@@ -0,0 +1,41 @@
+//
+//  CoreDataManager.swift
+//  BudgetViewer
+//
+//  Created by  Andrii Moisol on 05.03.2021.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    
+    private init() {}
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "BudgetViewer")
+        
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        
+        return container
+    }()
+    
+    func saveContext() {
+        let context = CoreDataManager.shared.persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+}
