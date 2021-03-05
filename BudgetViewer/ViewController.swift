30a1fb443668ca865a6d81aebc0598860ec4f443
Andrii Moisol <andrii.moisol@gmail.com>
2021-03-05 21:12:10 +0200

    Initial commit

---
+++ BudgetViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudgetViewer
+//
+//  Created by  Andrii Moisol on 05.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
